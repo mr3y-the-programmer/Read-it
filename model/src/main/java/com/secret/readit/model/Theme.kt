9ea738837a3af841e7d93a3a3b936b0f63ff2c12
MR3Y <abdonasr379@gmail.com>
2020-07-02 23:08:55 +0200

    Add Theme Model

---
+++ model/src/main/java/com/secret/readit/model/Theme.kt
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.model
+
+data class Theme(val type: ThemeType,
+                 val isSelected: Boolean = false)
+
+
+
+enum class ThemeType(val label: String){
+    LIGHT("light"),
+    DARK("dark"),
+    SYSTEM("system"),
+    BATTERY_SAVER("battery_saver")
+}
