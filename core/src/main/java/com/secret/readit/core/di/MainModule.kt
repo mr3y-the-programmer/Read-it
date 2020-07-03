cff7cb280c8461a2d12a82e912b09f7386448f3b
MR3Y <abdonasr379@gmail.com>
2020-07-03 18:19:03 +0200

    Provide firestore by dagger module

---
+++ core/src/main/java/com/secret/readit/core/di/MainModule.kt
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class MainModule {
+
+    @Provides
+    @Singleton
+    fun provideFirestore(): FirebaseFirestore{
+        val firestore = FirebaseFirestore.getInstance()
+
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .build()
+        return firestore
+    }
+
+}
