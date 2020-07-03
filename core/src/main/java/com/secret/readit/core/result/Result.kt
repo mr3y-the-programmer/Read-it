6ffa6bee89df456d2b4af30939087ee651541c2c
MR3Y <abdonasr379@gmail.com>
2020-07-03 17:24:04 +0200

    Add Result sealed class

---
+++ core/src/main/java/com/secret/readit/core/result/Result.kt
@@ -0,0 +1,15 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.result
+
+sealed class Result<out R> {
+
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+}
