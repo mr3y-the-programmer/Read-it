3ce4e570966d0bf82f69ac7db610e807670210cc
MR3Y <abdonasr379@gmail.com>
2020-07-04 23:52:51 +0200

    Add Blueprint for Categories

---
+++ core/src/main/java/com/secret/readit/core/data/categories/CategoryDataSource.kt
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.data.categories
+
+import com.secret.readit.core.result.Result
+import com.secret.readit.model.Category
+import com.secret.readit.model.articleId
+
+/**
+ * Blueprint for basic operations on categories
+ */
+interface CategoryDataSource {
+
+    /**
+     * get All categories stored in firestore
+     */
+    fun getCategories(): Result<List<Category>>
+
+    /**
+     * get categories of specified [articleId]
+     */
+    fun getArticleCategories(id: articleId): Result<List<Category>>
+}
