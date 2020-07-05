100a7548a4a415d496190b5d122ee1b11568cd6d
MR3Y <abdonasr379@gmail.com>
2020-07-05 18:38:10 +0200

    Update Publisher Blueprint

--- core/src/main/java/com/secret/readit/core/data/publisher/PublisherInfoDataSource.kt
+++ core/src/main/java/com/secret/readit/core/data/publisher/PublisherInfoDataSource.kt
@@ -8,6 +8,7 @@
 package com.secret.readit.core.data.publisher
 
 import android.net.Uri
+import com.secret.readit.core.result.Result
 import com.secret.readit.model.articleId
 import com.secret.readit.model.publisherId
 
@@ -19,72 +20,84 @@ interface PublisherInfoDataSource {
     /**
      * get UniqueId of registered user
      */
-    suspend fun getId(): String
+    suspend fun getId(): Result<String>
 
     /**
      * get the name which will be displayed to other users
      */
-    suspend fun getDisplayName(): String
+    suspend fun getDisplayName(): Result<String>
 
     //TODO: update return type
     /**
      * get Uri of img stored in firestore
      */
-    suspend fun getProfileImgUri(): Uri
+    suspend fun getProfileImgUri(): Result<Uri>
 
     /**
      * get registered user email address
      */
-    suspend fun getEmailAddress(): String
+    suspend fun getEmailAddress(): Result<String>
 
     /**
      * the first time he signed, joined app
      */
-    suspend fun getCreatedSince(): Long
+    suspend fun getCreatedSince(): Result<Long>
 
     /**
      * ids of articles he published
      */
-    suspend fun getArticlesPublishedIds(): List<String>
+    suspend fun getArticlesPublishedIds(): Result<List<articleId>>
 
     /**
      * ids of categories he followed
      */
-    suspend fun getFollowedCategoriesIds(): List<String>
+    suspend fun getFollowedCategoriesIds(): Result<List<String>>
 
     /**
      * number of followers
      */
-    suspend fun getFollowersNumber(): Int
+    suspend fun getFollowersNumber(): Result<Int>
 
     /**
      * update publisher/user name
+     *
+     * @return true on success, otherwise false
      */
-    suspend fun setDisplayName(newName: String)
+    suspend fun setDisplayName(newName: String): Result<Boolean>
 
     //TODO: update param type
     /**
      * Update profileImg, nullability here means to use a placeholder or img used in sign-in identity provider
+     *
+     * @return true on success, otherwise false
      */
-    suspend fun setProfileImg(newImage: Byte?)
+    suspend fun setProfileImg(newImage: Byte?): Result<Boolean>
 
     /**
      * when publishing new article, add its id to publisher profile
+     *
+     * @return true on success, otherwise false
      */
-    suspend fun addNewArticleId(id: articleId)
+    suspend fun addNewArticleId(id: articleId): Result<Boolean>
 
     /**
      * when following new category, add its id to publisher profile
+     *
+     * @return true on success, otherwise false
      */
-    suspend fun addNewCategoryId(id: String)
+    suspend fun addNewCategoryId(id: String): Result<Boolean>
 
     /**
      * Increment number of followers, and add new follower to existing followers to be able to see his articles
+     *
+     * @return true on success, otherwise false
      */
-    suspend fun follow(id: publisherId)
+    suspend fun follow(id: publisherId): Result<Boolean>
 
     /**
      * decrement number of followers, and remove follower from existing followers
+     *
+     * @return true on success, otherwise false
      */
-    suspend fun unFollow(id: publisherId)
+    suspend fun unFollow(id: publisherId): Result<Boolean>
 }
