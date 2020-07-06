2f7ec23888c0607f7ab8bd47875d203ea093f6c8
MR3Y <abdonasr379@gmail.com>
2020-07-06 22:52:02 +0200

    Add Blueprint for AuthDataSource

---
+++ core/src/main/java/com/secret/readit/core/data/auth/AuthDataSource.kt
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.data.auth
+
+import android.net.Uri
+
+/**
+ * Blueprint for information we get from FirebaseAuth
+ */
+interface AuthDataSource {
+
+    /**
+     * get unique user id, This is Unique within entire firebase project.
+     * Don't use it to identify user, instead see [FirebaseUser.getToken()]
+     */
+    fun getUid(): String
+
+    /**
+     * get the name which will be displayed to other users
+     */
+    fun getDisplayName(): String
+
+    /**
+     * get Uri of account img
+     */
+    fun getProfileImgUri(): Uri
+
+    /**
+     * get registered user email address
+     */
+    fun getEmailAddress(): String
+
+    /**
+     * the first time he signed, joined app
+     */
+    fun getCreatedSince(): Long
+}
