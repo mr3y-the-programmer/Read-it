f49463a8a3cafeddc665d1eb426704bea6ab774c
MR3Y <abdonasr379@gmail.com>
2020-07-11 18:27:45 +0200

    Add CustomId mechanism

---
+++ core/src/main/java/com/secret/readit/core/data/articles/utils/CustomIDHandler.kt
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.data.articles.utils
+
+import com.secret.readit.model.Article
+import com.secret.readit.model.publisherId
+import timber.log.Timber
+import java.lang.IllegalArgumentException
+
+/**
+ * CustomIDHandler has one responsibility to get ids of each article
+ */
+class CustomIDHandler {
+
+    /**
+     * get an Id for each article
+     *
+     * @throws IllegalArgumentException if (timestamp, publisherId, title) of article wasn't valid
+     * @return the id generated
+     */
+    fun getID(article: Article): String{
+        val stringTimestamp = article.timestamp.toString()
+        val pubId = article.publisher.id
+        val title = article.title
+
+        if (title.length < 4 || stringTimestamp.length < 7 || pubId.length < 4) {
+            Timber.d("Cannot make id for article: $article")
+            throw IllegalArgumentException("Cannot make an id for Invalid article")
+        }
+        val first = stringTimestamp.substring(5, stringTimestamp.length)
+        val middle = pubId.substring(0, 4)
+        val last = title.substring(0, 4)
+
+        return "$first$DASH$middle$DASH$last"
+    }
+
+    companion object {
+        const val DASH = "-"
+    }
+}
---
+++ core/src/test/java/com/secret/readit/core/data/articles/utils/CustomIDHandlerTest.kt
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.data.articles.utils
+
+import com.google.common.truth.Truth.*
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.lang.IllegalArgumentException
+
+class CustomIDHandlerTest {
+
+    //Object under test
+    private lateinit var customIDHandler: CustomIDHandler
+
+    @Before
+    fun setUp() {
+        customIDHandler = CustomIDHandler()
+    }
+
+    @Test
+    fun inputArticle2_ReturnValidId() {
+        //When entering valid article
+        val result = customIDHandler.getID(TestData.article2)
+
+        val expected = "89479892-2pub-arti"
+
+        //assert it matches our expectations
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun inputArticle1_ReturnValidId() {
+        //When entering valid article
+        val result = customIDHandler.getID(TestData.article1)
+
+        val expected = "43259253-1pub-arti"
+
+        //assert it matches our expectations
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun inputInvalidArticle_ThrowsException() {
+        //When entering Invalid article
+        val result = customIDHandler.getID(TestData.emptyArticle)
+
+        //assert it throws an exception
+    }
+}
