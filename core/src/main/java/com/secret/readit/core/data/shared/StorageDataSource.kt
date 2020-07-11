b5e9dc7f4aea1d38a24efa76da15702293f4a934
MR3Y <abdonasr379@gmail.com>
2020-07-11 21:49:00 +0200

    Add Blueprint for Firebase Storage

---
+++ core/src/main/java/com/secret/readit/core/data/shared/StorageDataSource.kt
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.data.shared
+
+import android.net.Uri
+import com.secret.readit.core.result.Result
+import com.secret.readit.model.articleId
+import java.io.InputStream
+
+/**
+ * Blueprint for cloud storage operations for publishers/articles...etc
+ */
+interface StorageDataSource {
+
+    /**
+     * Upload the bitmap's input stream to path within 'articles/articleId/'
+     *
+     * @return the download Uri
+     */
+    fun uploadBitmap(id: articleId, inputStream: InputStream): Result<Uri>
+
+    /**
+     * Download bitmap that this uri represent
+     *
+     * @return bitmap's input stream
+     */
+    fun downloadBitmap(id: articleId, uri: Uri): Result<InputStream>
+}
