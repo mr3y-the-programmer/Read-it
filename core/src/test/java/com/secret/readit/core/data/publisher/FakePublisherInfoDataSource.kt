2ce64e91c49a917fa4ebad77a7c338ccfec2a0b9
MR3Y <abdonasr379@gmail.com>
2020-07-16 00:30:14 +0200

    Refactor code size in FakePublisherInfoDataSource

--- core/src/test/java/com/secret/readit/core/data/publisher/FakePublisherInfoDataSource.kt
+++ core/src/test/java/com/secret/readit/core/data/publisher/FakePublisherInfoDataSource.kt
@@ -14,39 +14,21 @@ import com.secret.readit.model.publisherId
 
 class FakePublisherInfoDataSource: PublisherInfoDataSource {
 
-    override suspend fun getPublisherId(publisher: PubImportantInfo): Result<publisherId> {
-        return Result.Success(TestData.publisher1.id)
-    }
+    override suspend fun getPublisherId(publisher: PubImportantInfo): Result<publisherId> = Result.Success(TestData.publisher1.id)
 
-    override suspend fun getPublisher(id: publisherId): Result<Publisher> {
-        return Result.Success(TestData.publisher1)
-    }
+    override suspend fun getPublisher(id: publisherId): Result<Publisher> = Result.Success(TestData.publisher1)
 
-    override suspend fun setDisplayName(newName: String, id: publisherId): Result<Boolean> {
-        return Result.Success(true)
-    }
+    override suspend fun setDisplayName(newName: String, id: publisherId): Result<Boolean> = Result.Success(true)
 
-    override suspend fun addNewArticleId(articleID: articleId, publisherID: publisherId): Result<Boolean> {
-        return Result.Success(true)
-    }
+    override suspend fun addNewArticleId(articleID: articleId, publisherID: publisherId): Result<Boolean> =  Result.Success(true)
 
-    override suspend fun removeExistingArticleId(articleID: articleId, publisherID: publisherId): Result<Boolean> {
-        return Result.Success(true)
-    }
+    override suspend fun removeExistingArticleId(articleID: articleId, publisherID: publisherId): Result<Boolean> = Result.Success(true)
 
-    override suspend fun addNewCategoryId(categoryID: String, publisherID: publisherId): Result<Boolean> {
-        return Result.Success(true)
-    }
+    override suspend fun addNewCategoryId(categoryID: String, publisherID: publisherId): Result<Boolean> = Result.Success(true)
 
-    override suspend fun unFollowExistingCategoryId(categoryID: String, publisherID: publisherId): Result<Boolean> {
-        return Result.Success(true)
-    }
+    override suspend fun unFollowExistingCategoryId(categoryID: String, publisherID: publisherId): Result<Boolean> = Result.Success(true)
 
-    override suspend fun follow(followedPublisherID: publisherId, publisherID: publisherId): Result<Boolean> {
-        return Result.Success(true)
-    }
+    override suspend fun follow(followedPublisherID: publisherId, publisherID: publisherId): Result<Boolean> = Result.Success(true)
 
-    override suspend fun unFollow(unFollowedPublisherID: publisherId, publisherID: publisherId): Result<Boolean> {
-        return Result.Success(true)
-    }
+    override suspend fun unFollow(unFollowedPublisherID: publisherId, publisherID: publisherId): Result<Boolean> = Result.Success(true)
 }
