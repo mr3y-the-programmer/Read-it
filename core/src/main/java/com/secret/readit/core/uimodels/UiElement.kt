f82d3ca99a68a6b661c4154d8ddbce19300e8fe6
MR3Y <abdonasr379@gmail.com>
2020-07-16 01:35:24 +0200

    Add UiElement model

---
+++ core/src/main/java/com/secret/readit/core/uimodels/UiElement.kt
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.uimodels
+
+import android.graphics.Bitmap
+import com.secret.readit.model.BaseElement
+
+/**
+ * image Element's format that consumers expect to have
+ */
+data class ImageUiElement(val bitmap: Bitmap): BaseElement()
