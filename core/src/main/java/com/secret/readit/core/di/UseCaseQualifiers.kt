bd90e139bcaaf807a2c52af04a59808c346212a1
MR3Y <abdonasr379@gmail.com>
2020-07-21 01:52:21 +0200

    Add UseCaseQualifiers

---
+++ core/src/main/java/com/secret/readit/core/di/UseCaseQualifiers.kt
@@ -0,0 +1,14 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MostFollowedPublishers
