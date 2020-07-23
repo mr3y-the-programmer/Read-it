1fd77b07f0132456335973d85b3e8cd246b86af3
MR3Y <abdonasr379@gmail.com>
2020-07-24 01:38:46 +0200

    Add FakeCurrentUser

---
+++ core/src/test/java/com/secret/readit/core/domain/pubprofile/FakeCurrentUser.kt
@@ -0,0 +1,17 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.domain.pubprofile
+
+import com.secret.readit.core.domain.UseCase
+import com.secret.readit.core.uimodels.UiPublisher
+
+class FakeCurrentUser: UseCase<Unit, UiPublisher>(){
+    override suspend fun execute(parameters: Unit): UiPublisher {
+        return TestData.uiPublisher1
+    }
+}
