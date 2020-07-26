15a02a0a0d76e4a7424ce837185dfbf182fd7019
MR3Y <abdonasr379@gmail.com>
2020-07-26 18:29:28 +0200

    Implement DefaultContentDataSource

---
+++ core/src/main/java/com/secret/readit/core/data/articles/content/DefaultContentDataSource.kt
@@ -0,0 +1,74 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.data.articles.content
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.secret.readit.core.data.utils.wrapInCoroutineCancellable
+import com.secret.readit.core.di.IoDispatcher
+import com.secret.readit.core.result.Result
+import com.secret.readit.model.Element
+import com.secret.readit.model.articleId
+import kotlinx.coroutines.CoroutineDispatcher
+import timber.log.Timber
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+/**
+ * Our ContentDataSource has one responsibility, interact directly with firebase to get/set content of article
+ */
+internal class DefaultContentDataSource @Inject constructor(private val firestore: FirebaseFirestore,
+                                                   @IoDispatcher private val ioDispatcher: CoroutineDispatcher): ContentDataSource {
+    override suspend fun getContent(id: articleId): Result<List<Element>> {
+        return wrapInCoroutineCancellable(ioDispatcher) { continuation ->
+            firestore.collection(ARTICLES_COLLECTION)
+                .document(id)
+                .collection(CONTENT_COLLECTION)
+                .get()
+                .addOnSuccessListener { contentSnapshot ->
+                    if (continuation.isActive) {
+                        Timber.d("fetched content of article: $id Successfully, content: ${contentSnapshot.documents}")
+
+                        val content = contentSnapshot.toObjects(Element::class.java)
+
+                        continuation.resume(Result.Success(content))
+                    } else {
+                        Timber.d("Exception, continuation is no longer active")
+                    }
+                }.addOnFailureListener {
+                    Timber.d("Exception in fetching content, Cause: ${it.message}")
+                    continuation.resumeWithException(it)
+                }
+        }
+    }
+
+    override suspend fun addContent(id: articleId, elements: List<Element>): Result<Boolean> {
+        return wrapInCoroutineCancellable(ioDispatcher) { continuation ->
+            firestore.collection(ARTICLES_COLLECTION)
+                .document(id)
+                .collection(CONTENT_COLLECTION)
+                .add(elements)
+                .addOnSuccessListener {
+                    if (continuation.isActive) {
+                        Timber.d("Added content of article: $id Successfully")
+                        continuation.resume(Result.Success(true))
+                    } else {
+                        Timber.d("Exception, continuation is no longer active")
+                    }
+                }.addOnFailureListener {
+                    Timber.d("Exception in adding article: $id content, Cause: ${it.message}")
+                    continuation.resumeWithException(it)
+                }
+        }
+    }
+
+    companion object {
+        const val ARTICLES_COLLECTION = "articles"
+        const val CONTENT_COLLECTION = "content"
+    }
+}
