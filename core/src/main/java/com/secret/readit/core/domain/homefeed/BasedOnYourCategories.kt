55a83b7f7fa9824785d45a6ecbd06c10851d1045
MR3Y <abdonasr379@gmail.com>
2020-07-26 20:39:01 +0200

    Update BasedOnYourCategories UseCase

--- core/src/main/java/com/secret/readit/core/domain/homefeed/BasedOnYourCategories.kt
+++ core/src/main/java/com/secret/readit/core/domain/homefeed/BasedOnYourCategories.kt
@@ -10,21 +10,24 @@ package com.secret.readit.core.domain.homefeed
 import com.secret.readit.core.data.articles.ArticlesRepository
 import com.secret.readit.core.data.publisher.PublisherRepository
 import com.secret.readit.core.domain.FlowUseCase
-import com.secret.readit.model.Article
-import kotlinx.coroutines.flow.*
+import com.secret.readit.core.uimodels.UiArticle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.filterNot
 import javax.inject.Inject
 
 /**
  * Identical to [FromPublishersYouFollow] but for categories
  */
 class BasedOnYourCategories @Inject constructor(private val pubRepo: PublisherRepository,
-                                                private val articlesRepo: ArticlesRepository): FlowUseCase<Int, Article>() {
-    override suspend fun execute(parameters: Int): Flow<Article> {
+                                                private val articlesRepo: ArticlesRepository): FlowUseCase<Int, UiArticle>() {
+    override suspend fun execute(parameters: Int): Flow<UiArticle> {
         val limit = parameters.coerceIn(5, 30) // Ensure we don't request big number of articles that user will never read
         val categoryFollowedIds = pubRepo.getCurrentUser().publisher.followedCategoriesIds
 
         return articlesRepo.getArticlesWhichHaveCategories(limit, categoryFollowedIds).asFlow()
-            .filterNot { it.id.isEmpty() || it.timestamp < 0 }
+            .filterNot { it.article.id.isEmpty() || it.article.timestamp < 0 }
             .cancellable() // asFlow is unSafeFlow so we need to check the cancellation by using cancellable()
     }
 }
