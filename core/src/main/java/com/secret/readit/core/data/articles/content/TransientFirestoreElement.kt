81e81c73b49e9d8b8cc2a9e09cd9857fb58746a1
MR3Y <abdonasr379@gmail.com>
2020-07-29 01:00:00 +0200

    Add TemporaryFirestoreElement

---
+++ core/src/main/java/com/secret/readit/core/data/articles/content/TransientFirestoreElement.kt
@@ -0,0 +1,15 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.data.articles.content
+
+/**
+ * This just a temporary(transient) Element model which firestore can store
+ */
+data class TransientFirestoreElement(val text: String?,
+                                     val markup: Map<String, String>?,
+                                     val imgUri: String?)
