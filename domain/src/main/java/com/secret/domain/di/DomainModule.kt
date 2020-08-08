71eb2ff5aff6133fa927ef911f573d21c3c0b5e9
MR3Y <abdonasr379@gmail.com>
2020-08-08 20:33:28 +0200

    Add missing dagger stuff(WORK_INPROGRESS)

---
+++ domain/src/main/java/com/secret/domain/di/DomainModule.kt
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.domain.di
+
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DomainModule {
+    @Provides
+    @Singleton
+    fun provideClientSearch(appId: String, searchApiKey: String): ClientSearch { //TODO: make a qualifier for params and fetch them with RemoteConfig
+        return ClientSearch(ApplicationID(appId), APIKey(searchApiKey))
+    }
+}
