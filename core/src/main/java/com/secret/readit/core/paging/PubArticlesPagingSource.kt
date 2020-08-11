15bf0c8ba573d39c54c6f796ce0e0ee7e399710c
MR3Y <abdonasr379@gmail.com>
2020-08-12 01:45:52 +0200

    Add PubArticlesPagingSource

---
+++ core/src/main/java/com/secret/readit/core/paging/PubArticlesPagingSource.kt
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.paging
+
+import androidx.paging.PagingSource
+import com.google.firebase.firestore.DocumentSnapshot
+import com.secret.readit.core.data.articles.ArticlesDataSource
+import com.secret.readit.core.data.articles.content.ContentDataSource
+import javax.inject.Inject
+
+/**
+ * Identical to [ArticlesPagingSource] but For articles with specific publisher
+ * **NOTE**: This should be cached By appropriate scope of Ui consumers like: viewModelScope
+ */
+//TODO: handle the lifetime of this Source in dagger
+class PubArticlesPagingSource @Inject constructor(private val reqParams: RequestParams,
+                                                  private val articlesSource: ArticlesDataSource,
+                                                  private val contentSource: ContentDataSource): PagingSource<DocumentSnapshot, ArticleWithContent>() {
+
+    override suspend fun load(params: LoadParams<DocumentSnapshot>): LoadResult<DocumentSnapshot, ArticleWithContent> {
+        val result = articlesSource.getPubArticles(reqParams.specificPub, params.key)
+        return process(result, params, contentSource, reqParams.contentLimit)
+    }
+}
