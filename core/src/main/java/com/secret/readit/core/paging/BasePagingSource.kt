69ff063ea3c92083a237b9bba978225613a7ad5b
MR3Y <abdonasr379@gmail.com>
2020-08-12 21:06:39 +0200

    Add BasePagingSource interface

---
+++ core/src/main/java/com/secret/readit/core/paging/BasePagingSource.kt
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.paging
+
+import androidx.paging.PagingSource
+import com.google.firebase.firestore.DocumentSnapshot
+
+/**
+ * This is the Base PagingSource that handles childs boilerplate
+ */
+interface BasePagingSource {
+
+    /**
+     * Specify params needed to make request of data like limit, ids...etc
+     */
+    var reqParams: RequestParams
+
+    /**
+     * Handles providing request data to PagingSource
+     */
+    @Suppress("UNCHECKED_CAST")
+    fun withParams(reqParams: RequestParams): PagingSource<DocumentSnapshot, ArticleWithContent> {
+        val source = apply { this.reqParams = reqParams }
+        return (source as PagingSource<DocumentSnapshot, ArticleWithContent>)
+    }
+}
