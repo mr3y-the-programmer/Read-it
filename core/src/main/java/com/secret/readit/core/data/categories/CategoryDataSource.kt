f078cbde89d23cdf8f6a812383d9f2f7cf3d2ac5
MR3Y <abdonasr379@gmail.com>
2020-08-13 21:09:03 +0200

    Update getCategories() params in dataSource

--- core/src/main/java/com/secret/readit/core/data/categories/CategoryDataSource.kt
+++ core/src/main/java/com/secret/readit/core/data/categories/CategoryDataSource.kt
@@ -7,6 +7,7 @@
 
 package com.secret.readit.core.data.categories
 
+import com.google.firebase.firestore.DocumentSnapshot
 import com.secret.readit.core.result.Result
 import com.secret.readit.model.Category
 import com.secret.readit.model.articleId
@@ -18,13 +19,15 @@ interface CategoryDataSource {
 
     /**
      * the main fun to get categories stored in Categories collection,
-     * it takes additional parameters like [ids] to customize query,
+     * it takes additional parameters like [ids], [limit] to customize query,
+     * it also support Pagination by taking the [prevSnapshot] param
      * if all parameters empty it will return the whole list of categories
      */
-    suspend fun getCategories(ids: List<String>): Result<List<Category>>
+    suspend fun getCategories(limit: Int, ids: List<String>, prevSnapshot: DocumentSnapshot?): Result<Pair<List<Category>, DocumentSnapshot>>
 
     /**
      * get categories of specified [articleId]
      */
+    //TODO: This is UnUsed and maybe Removed later
     suspend fun getArticleCategories(id: articleId): Result<List<Category>>
 }
