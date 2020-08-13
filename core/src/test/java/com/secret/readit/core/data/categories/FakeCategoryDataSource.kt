c335483a37216265d837f34a8a1985db9582a054
MR3Y <abdonasr379@gmail.com>
2020-08-13 21:10:06 +0200

    Update FakeCategoryDataSource

--- core/src/test/java/com/secret/readit/core/data/categories/FakeCategoryDataSource.kt
+++ core/src/test/java/com/secret/readit/core/data/categories/FakeCategoryDataSource.kt
@@ -7,14 +7,20 @@
 
 package com.secret.readit.core.data.categories
 
+import com.google.firebase.firestore.DocumentSnapshot
+import com.nhaarman.mockitokotlin2.mock
 import com.secret.readit.core.TestData
 import com.secret.readit.core.result.Result
 import com.secret.readit.model.Category
 import com.secret.readit.model.articleId
 
 open class FakeCategoryDataSource : CategoryDataSource {
-    override suspend fun getCategories(ids: List<String>): Result<List<Category>> {
-        return Result.Success(TestData.categories)
+    override suspend fun getCategories(
+        limit: Int,
+        ids: List<String>,
+        prevSnapshot: DocumentSnapshot?
+    ): Result<Pair<List<Category>, DocumentSnapshot>> {
+        return Result.Success(Pair(TestData.categories, mock {  }))
     }
 
     override suspend fun getArticleCategories(id: articleId): Result<List<Category>> {
