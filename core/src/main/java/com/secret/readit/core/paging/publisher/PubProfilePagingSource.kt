e365346ab3d9f9005c7c8f2b4dd1769754dd6618
MR3Y <abdonasr379@gmail.com>
2020-08-13 03:06:42 +0200

    Add PubProfilePagingSource

---
+++ core/src/main/java/com/secret/readit/core/paging/publisher/PubProfilePagingSource.kt
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.paging.publisher
+
+import androidx.paging.PagingSource
+import com.google.firebase.firestore.DocumentSnapshot
+import com.secret.readit.core.data.publisher.PublisherInfoDataSource
+import com.secret.readit.core.paging.BasePagingSource
+import com.secret.readit.model.Publisher
+import javax.inject.Inject
+
+/**
+ * Paging Source which loads/caches things related to Pub Profile like following list
+ */
+class PubProfilePagingSource @Inject constructor(
+    private val pubDataSource: PublisherInfoDataSource
+): PagingSource<DocumentSnapshot, Publisher>(), BasePagingSource<RequestParams> {
+
+    override var reqParams: RequestParams = RequestParams(0, 0, emptyList()) //It is consumer responsibility to fill the request
+
+    override suspend fun load(params: LoadParams<DocumentSnapshot>): LoadResult<DocumentSnapshot, Publisher> {
+        val result = pubDataSource.getPublishers(reqParams.withIds, reqParams.followersNum, reqParams.limit, params.key)
+        return process(result, params)
+    }
+}
