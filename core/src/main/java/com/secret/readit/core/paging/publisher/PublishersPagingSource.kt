383839b213b6d24587e17e2109d9b93c11fdb09a
MR3Y <abdonasr379@gmail.com>
2020-08-13 03:05:42 +0200

    Add PublisherPagingSource

---
+++ core/src/main/java/com/secret/readit/core/paging/publisher/PublishersPagingSource.kt
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.paging.publisher
+
+import androidx.paging.PagingSource
+import com.google.firebase.firestore.DocumentSnapshot
+import com.secret.readit.core.data.publisher.PublisherInfoDataSource
+import com.secret.readit.core.paging.BasePagingSource
+import com.secret.readit.core.paging.checkIfSuccessful
+import com.secret.readit.model.Publisher
+import javax.inject.Inject
+import kotlin.Exception
+
+/**
+ * Paging Source which handles loading/caching things related to publishers on homefeed..etc
+ */
+class PublishersPagingSource @Inject constructor(
+    private val pubDataSource: PublisherInfoDataSource
+): PagingSource<DocumentSnapshot, Publisher>(), BasePagingSource<RequestParams>{
+
+    override var reqParams: RequestParams = RequestParams(0, 0, emptyList()) //It is consumer responsibility to fill the request
+
+    override suspend fun load(params: LoadParams<DocumentSnapshot>): LoadResult<DocumentSnapshot, Publisher> {
+        val result = pubDataSource.getPublishers(reqParams.withIds, reqParams.followersNum, reqParams.limit, params.key)
+        return process(result, params)
+    }
+}
---
+++ core/src/main/java/com/secret/readit/core/paging/publisher/utils.kt
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.paging.publisher
+
+import androidx.paging.PagingSource
+import com.google.firebase.firestore.DocumentSnapshot
+import com.secret.readit.core.paging.checkIfSuccessful
+import com.secret.readit.core.result.Result
+import com.secret.readit.model.Publisher
+
+fun process(result: Result<Pair<List<Publisher>, DocumentSnapshot>>,
+            params: PagingSource.LoadParams<DocumentSnapshot>
+): PagingSource.LoadResult<DocumentSnapshot, Publisher> {
+    var lastSnapshot = params.key
+    val data = checkIfSuccessful(result)?.let {
+        lastSnapshot = it.second
+        it.first
+    } ?: return PagingSource.LoadResult.Error(Exception())
+    return PagingSource.LoadResult.Page(data, null /*We don't support paging in that direction*/, nextKey = lastSnapshot)
+}
