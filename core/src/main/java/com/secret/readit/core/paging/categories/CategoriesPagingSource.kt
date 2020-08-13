542739ee6cb7144c7bdac3bce34a07a2df2253b4
MR3Y <abdonasr379@gmail.com>
2020-08-13 22:28:53 +0200

    Add CategoriesPagingSource

---
+++ core/src/main/java/com/secret/readit/core/paging/categories/CategoriesPagingSource.kt
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.paging.categories
+
+import androidx.paging.PagingSource
+import com.google.firebase.firestore.DocumentSnapshot
+import com.secret.readit.core.data.categories.CategoryDataSource
+import com.secret.readit.core.paging.BasePagingSource
+import com.secret.readit.core.paging.publisher.process
+import com.secret.readit.model.Category
+import javax.inject.Inject
+
+/**
+ * PagingSource that loads categories in pages
+ */
+class CategoriesPagingSource @Inject constructor(
+    private val categoryDataSource: CategoryDataSource
+): PagingSource<DocumentSnapshot, Category>(), BasePagingSource<RequestParams> {
+
+    override var reqParams: RequestParams = RequestParams(0, emptyList()) //It is consumer responsibility to fill the request
+
+    override suspend fun load(params: LoadParams<DocumentSnapshot>): LoadResult<DocumentSnapshot, Category> {
+        val result = categoryDataSource.getCategories(reqParams.limit, reqParams.ids, params.key)
+        return process(result, params)
+    }
+}
---
+++ core/src/main/java/com/secret/readit/core/paging/categories/RequestParams.kt
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.readit.core.paging.categories
+
+import com.secret.readit.core.paging.BaseReqParams
+
+/**
+ * Request Params for Category PagingSource
+ */
+data class RequestParams(val limit: Int,
+                         val ids: List<String>) : BaseReqParams()
