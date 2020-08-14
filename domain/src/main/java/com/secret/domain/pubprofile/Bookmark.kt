aa34b2b9c9d2b956a600865b2d72c2eb6dc0787a
MR3Y <abdonasr379@gmail.com>
2020-08-14 20:02:21 +0200

    Add Bookmark UseCase

---
+++ domain/src/main/java/com/secret/domain/pubprofile/Bookmark.kt
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (C) MR3Y - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly prohibited
+ *   Proprietary and confidential
+ *   Written by MR3Y <abdonasr379@gmail.com>, 2020.
+ */
+
+package com.secret.domain.pubprofile
+
+import com.secret.domain.FlowUseCase
+import com.secret.readit.core.data.publisher.PublisherRepository
+import com.secret.readit.core.uimodels.UiArticle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+/**
+ * Add Article to Bookmarks UseCase
+ */
+class Bookmark @Inject constructor(private val pubRepo: PublisherRepository) : FlowUseCase<Pair<UiArticle, UpdateBookmark>, Boolean>() {
+    override suspend fun execute(parameters: Pair<UiArticle, UpdateBookmark>): Flow<Boolean> {
+        return flow {
+            when(parameters.second) {
+                UpdateBookmark.BOOKMARK -> emit(pubRepo.bookmark(parameters.first))
+                UpdateBookmark.UN_BOOKMARK -> emit(pubRepo.unBookmark(parameters.first))
+            }
+        }
+    }
+}
+
+/**
+ * Define whether to bookmark or unBookmark article
+ */
+enum class UpdateBookmark{
+    BOOKMARK,
+    UN_BOOKMARK
+}
