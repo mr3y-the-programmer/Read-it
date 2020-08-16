1522ca623a43b474858afad4cdb2d29f01319ff4
MR3Y <abdonasr379@gmail.com>
2020-08-16 03:07:20 +0200

    Safe delete search related code in domain

--- domain/src/main/java/com/secret/domain/di/DomainModule.kt
+++ domain/src/main/java/com/secret/domain/di/DomainModule.kt
@@ -7,18 +7,9 @@
 
 package com.secret.domain.di
 
-import com.algolia.search.client.ClientSearch
-import com.algolia.search.model.APIKey
-import com.algolia.search.model.ApplicationID
 import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
 
 @Module
 class DomainModule {
-    @Provides
-    @Singleton
-    fun provideClientSearch(appId: String, searchApiKey: String): ClientSearch { // TODO: make a qualifier for params and fetch them with RemoteConfig
-        return ClientSearch(ApplicationID(appId), APIKey(searchApiKey))
-    }
+
 }
