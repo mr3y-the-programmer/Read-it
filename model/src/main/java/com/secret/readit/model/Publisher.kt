61857595784b7f45879336f29b5ae3fc73fcb687
MR3Y <abdonasr379@gmail.com>
2020-08-25 01:41:09 +0200

    Add Rank field
    modify publisher model to include rank field which immutable For client, Configured only from server code

--- model/src/main/java/com/secret/readit/model/Publisher.kt
+++ model/src/main/java/com/secret/readit/model/Publisher.kt
@@ -18,4 +18,6 @@ data class Publisher(val id: publisherId,
                      val followedCategoriesIds: List<String> = emptyList(),
                      val followedPublishersIds: List<publisherId> = emptyList(),
                      val bookmarkedArticlesIds: List<articleId> = emptyList(),
-                     val numOfFollowers: Int = 0)
+                     val numOfFollowers: Int = 0,
+                     val rank: String = "Beginner") //This is immutable for client, it has one of values: ["Beginner", "amateur", "Pro", "Super_Pro"]
+           // TODO: Configure this from server through cloud function, fields affect rank: memberSince, publishedArticles
